8cf69578106a6ef7b4988ca03e79826650b204e0
Joe Kanter <kanterjoe@gmail.com>
2018-06-22 10:41:11 -0500

    finished

---
+++ index.js
@@ -0,0 +1,40 @@
+var bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+var PORT = process.env.PORT || 3000;
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use( bodyParser.urlencoded({ extended: false }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+const texts = [];
+app.post('/texts', (req, res) => {
+    
+    if (req.body.text) {
+        texts.push(req.body.text);
+        console.log("success")
+        res.send(req.body);
+
+    }
+    else {
+        res.send(400);
+
+    }
+
+
+})
+app.get('/texts', (req,res)=>{
+    console.log(texts);
+    let ps = texts.map(i=>`<p>${i}</p><hr>`)
+    res.send(ps.reduce ((acc, item) =>{
+        return `${acc} ${item}`
+    },'')); 
+})
+
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
